51da20ea2f77dcab5f9cc3176f7dcc862a3db011
Dunemask <dunemask@gmail.com>
2025-09-16 16:24:04 -0600

    [CHORE] Working Failing Coverage

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
+  { ignores: ["coverage/**"] },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
 ]);
