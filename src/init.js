bd5fe4e619ea531983e3a2f5155efb6c5a4ad4a2
Dunemask <dunemask@gmail.com>
2025-09-24 19:51:12 -0600

    [FEATURE] 80% Coverage & CI

--- src/init.js
+++ src/init.js
@@ -9,5 +9,8 @@ const name = process.argv[2];
 const email = process.argv[3];
 const password = process.argv[4];
 const user = { name, email, password, roles: [{ role: Role.Admin }] };
-DB.addUser(user).then((r) => console.log("created user: ", r));
-DB.addMenuItem({ "title": "Crusty CI", "description": "A CI favorite", "image": "pizza1.png", "price": 0.028 });
+async function main() {
+  await DB.addUser(user).then((r) => console.log("created user: ", r));
+  await DB.addMenuItem({ "title": "Crusty CI", "description": "A CI favorite", "image": "pizza1.png", "price": 0.028 });
+}
+main().catch((err) => console.error(err));
