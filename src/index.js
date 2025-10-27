d4d6beb74dfb9bcbe16f6c1a8eed6c80906e9da4
Dunemask <dunemask@gmail.com>
2025-10-27 17:14:09 -0600

    [CHORE] Test App

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
-const app = require("./service.js");
+// const app = require("./service.js");
+
+const express = require("express");
+const app = express();
+app.get("/healthz", (_, res) => res.sendStatus(200));
+app.get("/livez", (_, res) => res.sendStatus(200));
 
 const port = process.argv[2] || 3000;
 app.listen(port, () => {
