9b24f6142b894e99696a8d48991ac8368da8c105
Dunemask <dunemask@gmail.com>
2025-10-27 17:18:36 -0600

    [CHORE] Test App

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const app = express();
 app.get("/healthz", (_, res) => res.sendStatus(200));
 app.get("/livez", (_, res) => res.sendStatus(200));
 
+
 const port = process.argv[2] || 3000;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
