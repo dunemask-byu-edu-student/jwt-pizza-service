e7e5ffe4c36c44c2a67e721acdfd906614df0ea3
Dunemask <dunemask@gmail.com>
2025-10-27 20:29:03 -0600

    [CHORE] Integrate Healthz Endpoint

--- src/routes/vitalsRouter.js
+++ src/routes/vitalsRouter.js
@@ -4,4 +4,7 @@ const vitalsRouter = express.Router();
 vitalsRouter.get("/livez", (_req, res) => {
   res.sendStatus(200);
 });
+vitalsRouter.get("/healthz", (_req, res) => {
+  res.sendStatus(200);
+});
 module.exports = vitalsRouter;
